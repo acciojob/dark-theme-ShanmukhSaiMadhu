c8aaf3dabf91b2f19a07ca4c96cbe92d8bd43a24
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-07-18 18:27:24 +0530

    Done

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here
+let swap = document.getElementById('swap');
+let app = document.getElementById('app');
 
+
+ swap.addEventListener('click', () => {
+    // document.getElementById('app').classList = 'night';
+    // document.getElementById('swap').classList = 'button_night';
+
+        if(app.classList.contains('day') && swap.classList.contains('button_day')) {
+            document.getElementById('app').classList = 'night';
+            document.getElementById('swap').classList = 'button_night';
+        } else if(app.classList.contains('night') && swap.classList.contains('button_night')) {
+            document.getElementById('app').classList = 'day';
+            document.getElementById('swap').classList = 'button_day';
+        }
+});
